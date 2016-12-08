90076d06b84ed91ff472a286122ab0b9c5a795c5
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-12-08 19:44:32 +0700

    add server.js (express configuration)/ PORT SERVER : 8080

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+// JSON Web Tokens
+const jwt = require('jsonwebtoken')
+
+// Authentication
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+const PORT = process.env.PORT || 8080
+
+app.use(cors())
+app.use(logger())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.listen(PORT, (err) => {
+  if(err){
+    console.error(err);
+  }else{
+    console.log(`server is running in port ${PORT}`);
+  }
+})
