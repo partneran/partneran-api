527c6010e6d7c25552b5c676ee09c3daec58c21f
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-12-12 13:52:24 +0700

    api votes controller

--- routes/routes.api.votes.js
+++ routes/routes.api.votes.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const controller = require('../controllers/controllers.api.comments')
+const controller = require('../controllers/controllers.api.votes')
 
 const router = express.Router()
 
 router.post('/:ideaid/votes', controller.upVote)
-router.get('/:ideaid/votes', controller.getCountVote)
+router.get('/:ideaid/votes/:voteid', controller.getCountVote)
 router.get('/:ideaid/votes/:voteid', controller.downVote)
 
 module.exports = router;
