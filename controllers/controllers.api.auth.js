c38e1f7e25825942bef8e692c8c615cd9d1a3bc9
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-12-13 16:41:52 +0700

    config email signup users

--- controllers/controllers.api.auth.js
+++ controllers/controllers.api.auth.js
@@ -46,56 +46,100 @@ let testingSignUp = (req, res, next) => {
   * method : POST
 */
 let signUpUser = (req, res, next) => {
-  var token = jwt.sign({
-                sub: req.body.id,
-                name: req.body.name,
-                email: req.body.email,
-                // photo_URL: req.body.photo_URL,
-                verify: false,
-                isSuper: 'super'
-            }, process.env.SECRET_TOKEN, { expiresIn: 60*60 }) // expire in 1 hour
-
-  // console.log(process.env.SENDGRID_API_KEY);
-  var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-  var request = sg.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: {
-      personalizations: [
-        {
-          to: [
-            {
-              email: req.body.email,
-            },
-          ],
-          subject: 'Hello World from the SendGrid Node.js Library!',
-        },
-      ],
-      from: {
-        email: 'no-reply@partneran.net',
-      },
-      content: [
-        {
-          type: 'text/plain',
-          value: 'Hello, Email!',
-        },
-      ],
-    },
-  });
-
   if(req.body.password.length >= 8){
-      //With promise
-      sg.API(request)
-        .then(response => {
-          console.log(response.statusCode);
-          console.log(response.body);
-          console.log(response.headers);
-        })
-        .catch(error => {
-          //error is an instance of SendGridError
-          //The full response is attached to error.response
-          console.log(error.response.statusCode);
-        });
+      //register users
+      Users
+        .register({
+            userId: req.body.userId,
+            email: req.body.email,
+            name: req.body.name,
+            verify: false,
+            isSuper: 'user',
+            status: 'baby'
+          }, req.body.password, (err, new_user) => {
+            if(err){
+              console.log(err);
+              res.status(400).json(err)
+            }else {
+              // res.json(new_user)
+              var token = jwt.sign({
+                            sub: new_user.id,
+                            name: req.body.name,
+                            email: req.body.email,
+                            // photo_URL: req.body.photo_URL,
+                            verify: false,
+                            isSuper: 'super'
+                        }, process.env.SECRET_TOKEN, { expiresIn: 60*60 }) // expire in 1 hour
+
+              // console.log(process.env.SENDGRID_API_KEY);
+              var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+              var request = sg.emptyRequest({
+                method: 'POST',
+                path: '/v3/mail/send',
+                body: {
+                  personalizations: [
+                    {
+                      to: [
+                        {
+                          email: req.body.email,
+                        },
+                      ],
+                      subject: 'Hello '+req.body.name+' Welcome to partneran.net, please verify your account',
+                    },
+                  ],
+                  from: {
+                    email: 'no-reply@partneran.net',
+                  },
+                  content: [
+                    {
+                      type: 'text/html',
+                      value: `Hello ${req.body.name}, Thankyou for register in <a href="http://partneran.net" target="_blank">partneran.net</a><br /><br />
+                      Click this link below to verify:<br /> <a href="http://localhost:8080/api/auth/verification/${token}">Verify Your Account</a>`,
+                    },
+                  ],
+                },
+              });
+
+            sg.API(request)
+              .then(response => {
+                // console.log(response.statusCode);
+                // console.log(response.body);
+                // console.log(response.headers);
+                passport.authenticate('local', {}, (err, user, info) => {
+                 if(err){
+                   console.log(`error`);
+                   console.log(err);
+                   return res.status(400).json(err)
+                 }else{
+                   if(user != false){
+                     // console.log(user);
+                     return res.status(200).json({
+                       token: jwt.sign({
+                         sub: user.id,
+                         name: user.name,
+                         email: user.email,
+                         photo_URL: user.photo_URL,
+                         short_bio: user.short_bio,
+                         verify: user.verify,
+                         isSuper: user.isSuper,
+                         status: user.status
+                       }, process.env.SECRET_TOKEN, { expiresIn: 60*60 })
+                     })
+                   }else{
+                     console.log(`info`);
+                     return res.status(400).json(info)
+                   }
+                 }
+               })(req, res, next)
+              })
+              .catch(error => {
+                //error is an instance of SendGridError
+                //The full response is attached to error.response
+                // console.log(error.response.statusCode);
+                res.json(error)
+              });
+            }
+          })
   }else{
     res.json({
       message: "Password must be 8 characters or more"
@@ -139,12 +183,12 @@ let loginUser = (req, res, next) => {
 */
 let verificationNewUser = (req, res) => {
   var decoded = jwt_decode(req.params.token)
-  // console.log(decoded);
+  console.log(decoded);
 
   Users
     .findOne({
       where: {
-        id: decoded.sub
+        email: decoded.email
       }
     })
     .then((one_data, err) => {
@@ -153,7 +197,7 @@ let verificationNewUser = (req, res) => {
         res.json(err)
       }else{
         // console.log(one_data);
-        one_data.verify= true
+        one_data.verify = true
         one_data.save()
         res.json(one_data)
       }
